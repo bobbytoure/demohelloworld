19ff0d87760237207d7e447f221e3a88ecda0f6f
Bobby Touré <bobby@Bobbys-MacBook-Pro.local>
2019-05-26 19:44:47 -0400

    new repository

---
+++ src/main/java/com/bobbyonline/demohelloworld/DemohelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.bobbyonline.demohelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemohelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemohelloworldApplication.class, args);
+	}
+
+}
